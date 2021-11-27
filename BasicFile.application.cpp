70386073c60a481714ab906ea10db96e2b871ba4
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-27 16:30:38 +0530

    BasicFile.application.cpp

---
+++ BasicFile.application.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b;
+ clrscr();
+  printf("Enter Value a&b= ");
+  scanf("%d %d",&a,&b);
+  if(a<b)
+  {
+    printf("A is Smallest");
+  }
+  else
+  {
+    printf("B is Smallest");
+  }
+ getch();
+}
